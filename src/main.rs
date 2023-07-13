54cc9c1c28717751f5d17f73425ecc58fe1a72fa
ashikmeerankutty <ashik9591@gmail.com>
2023-07-14 00:56:19 +0530

    Only run if the names dont match

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use serde::Deserialize;
 use std::fs::File;
 use std::process::Command;
-use serde::Deserialize;
 extern crate dirs;
 
 #[derive(Debug, Deserialize)]
@@ -16,42 +16,62 @@ fn main() {
 
     let config_file_path = home_dir.join(".git-switch.json");
 
-    let config_file = File::open(config_file_path).expect("Config not found, Create a .git-switch.json file in your home directory.");
+    let config_file = File::open(config_file_path)
+        .expect("Config not found, Create a .git-switch.json file in your home directory.");
 
-    let configs: Vec<GitConfig> = serde_json
-        ::from_reader(config_file)
-        .expect("Failed to read the config file");
+    let configs: Vec<GitConfig> =
+        serde_json::from_reader(config_file).expect("Failed to read the config file");
 
     for config in configs {
-        let output = Command::new("git")
+        let output: std::process::Output = Command::new("git")
             .arg("remote")
             .arg("get-url")
             .arg("origin")
             .output()
             .expect("Failed to execute 'git remote get-url origin'");
 
-        let origin_url = String::from_utf8_lossy(&output.stdout);
+        let origin_url: std::borrow::Cow<'_, str> = String::from_utf8_lossy(&output.stdout);
 
-        if origin_url.contains(&config.host) {
-            let username_output = Command::new("git")
-                .arg("config")
-                .arg("--global")
-                .arg("user.name")
-                .arg(&config.username)
-                .output()
-                .expect("Failed to execute 'git config --global user.name'");
-            let email_output = Command::new("git")
-                .arg("config")
-                .arg("--global")
-                .arg("user.email")
-                .arg(&config.email)
-                .output()
-                .expect("Failed to execute 'git config --global user.email'");
+        let current_user_name_output: std::process::Output = Command::new("git")
+            .arg("config")
+            .arg("user.name")
+            .output()
+            .expect("Failed to execute 'git config user.name'");
 
-            if username_output.status.success() && email_output.status.success() {
-                println!("Successfully switched Git configuration to: {:?}", config);
-            } else {
-                println!("Failed to switch Git configuration");
+        let current_user_email_output: std::process::Output = Command::new("git")
+            .arg("config")
+            .arg("user.email")
+            .output()
+            .expect("Failed to execute 'git config user.email'");
+
+        let current_user_name: std::borrow::Cow<'_, str> =
+            String::from_utf8_lossy(&current_user_name_output.stdout);
+
+        let current_user_email: std::borrow::Cow<'_, str> =
+            String::from_utf8_lossy(&current_user_email_output.stdout);
+
+        if current_user_name != config.username || current_user_email != config.email {
+            if origin_url.contains(&config.host) {
+                let username_output = Command::new("git")
+                    .arg("config")
+                    .arg("--global")
+                    .arg("user.name")
+                    .arg(&config.username)
+                    .output()
+                    .expect("Failed to execute 'git config --global user.name'");
+                let email_output = Command::new("git")
+                    .arg("config")
+                    .arg("--global")
+                    .arg("user.email")
+                    .arg(&config.email)
+                    .output()
+                    .expect("Failed to execute 'git config --global user.email'");
+
+                if username_output.status.success() && email_output.status.success() {
+                    println!("Successfully switched Git configuration to: {:?}", config);
+                } else {
+                    println!("Failed to switch Git configuration");
+                }
             }
         }
     }
